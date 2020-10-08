a08b8e0921dbcbd119784c96efe6b4d4fdb5460d
freddy <>
2020-10-08 10:23:03 +0800

    url解决方案，启动dev代理

---
+++ vueblog-vue/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'http://localhost:8081',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': '/', // rewrite path
+                    '^/api/remove/path': '/path', // remove base path
+                },
+            },
+            '^/foo': {
+                target: '<other_url>'
+            }
+        }
+    }
+}
